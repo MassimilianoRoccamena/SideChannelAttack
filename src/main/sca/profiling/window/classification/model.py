567ab148acce1c22cf5a9f7df3aef7e91cc0a3e8
MassimilianoRoccamena <roccamena@westworld>
2022-01-04 09:50:53 +0100

    remote fix

--- src/main/sca/profiling/window/classification/model.py
+++ src/main/sca/profiling/window/classification/model.py
@@ -2,6 +2,7 @@ from aidenv.api.dlearn.model import SingleClassifierModel
 from aidenv.api.dlearn.model import MultiClassifierModel
 from aidenv.api.dlearn.module.arch.resnet import ResNet
 from aidenv.api.dlearn.module.arch.lstm import LSTM1
+from aidenv.api.dlearn.module.arch.gru import GRU1
 from aidenv.api.dlearn.module.arch.transformer import Transformer
 
 # Basic : encoder + classifier
